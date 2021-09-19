e9f2c5e0f88a26e762f6f1befcd041c733c871a7
prodevmm <cdmmxo@gmail.com>
2021-09-19 20:36:19 +0630

    Initial Commit

--- xet3/src/main/java/com/bllsht/xet3/dto/Response.kt
+++ xet3/src/main/java/com/bllsht/xet3/dto/Response.kt
@@ -2,7 +2,7 @@ package com.bllsht.xet3.dto
 
 data class Response(val sdUrl: String, private val _hdUrl: String?, private val _hlsUrl: String?) {
     val hdUrl get() = _hdUrl ?: ""
-    val hlsUrl get() = _hdUrl ?: ""
+    val hlsUrl get() = _hlsUrl ?: ""
 
     fun isHDAvailable(): Boolean {
         return _hdUrl != null
